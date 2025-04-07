c2b1598a981af8ed5c2df8db4c3224aa3013dac5
VirekL <gk9735738@gmail.com>
2025-04-07 15:48:51 +0200

    s

--- js/components/Sidebar/RecordRules.js
+++ js/components/Sidebar/RecordRules.js
@@ -3,17 +3,20 @@ export default {
         <h3>Record Submission Requirements</h3>
         <div class="right-text">
             <p>
-                Vous devez avoir compléter le record sans utiliser de hacks (y compris les hacks qui changent la physique du jeu, ie. physics bypass via MegaHack, par contre, "Click Between Frames" est autorisé). 
+                Vous devez avoir envoyer votre record sans utiliser de hacks (y compris les hacks qui changent la physique du jeu, ie. physics bypass via MegaHack, par contre, "Click Between Frames" est autorisé). 
                 You must have achieved the record without using hacks (including hacks that change the physics of the game, ie. physics bypass via MegaHack, however, "Click Between Frames" is allowed).
             </p>
-            <p>
+            <p> 
+                Vous devez completer le niveau du qui est listé sur le site ou une version bugfix/ldm acceptée - veuiller check les id avant d'envoyer votre record
                 You must have achieved the record on the level that is listed on the site or on an approved bugfixed copy - please check the level ID before you submit a record!
             </p>
             <p>
+                Les records pour les completions Hard+ doivent avoir des clicks ou taps auditif ou visuelle (source audio est acceptable pour les utilisateurs d'iPhone). Cela n'inclus pas les clicks artificielles.
                 Records for Easy+ completions must have clicks or visual tap indication (source audio is acceptable for iPhone users). This does not include mods that add artificial click sounds.
             </p>
-            <p>
-                Complete raw footage is required alongside your record for any layouts in Extreme Tier or above.
+            <p> 
+                Les vidéos originelles completes (pas édit) sont requise pour les niveaux Extreme Tier ou au dessus.
+                Complete raw footage is required alongside your record for any levels in Extreme Tier or above.
             </p>
             <p>
                 The recording must have a previous attempt and death animation shown before the completion, unless the completion is on the first attempt.
