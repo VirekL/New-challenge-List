99e2a6ef8734e4c5effa3a9ed545abd25e0a9c37
VirekL <gk9735738@gmail.com>
2025-04-07 19:22:50 +0200

    j

--- js/components/Sidebar/RecordRules.js
+++ js/components/Sidebar/RecordRules.js
@@ -19,15 +19,19 @@ export default {
                 Complete raw footage is required alongside your record for any levels in Extreme Tier or above.
             </p>
             <p>
+                Votre complétion doit avoir un précédent essai et une animation de mort montré avant la complétion sauf si la complétion et faite au premier attemps
                 The recording must have a previous attempt and death animation shown before the completion, unless the completion is on the first attempt.
             </p>
             <p>
+                La vidéo doit montrer le joueur toucher le mur final et montrer les stats bien comme il faut
                 The recording must show the player hit the end-wall as well as present your end stats, or the completion will be invalidated.
             </p>
             <p>
-                Do not use secret routes, skips, or bug routes!
+                Ne pas utilisez des secret routes,skip ou bug routes qui ne sont pas autoriser !
+                Do not use secret routes, skips, or bug routes that not allowed !
             </p>
             <p>
+                Cheat Indicator est requis pour chaque completions via geode, MegaHack ou iCreate Pro. Si vous n'avez pas de Cheat Indicat,votre record a des chances d'être rejeté (cela ne s'applique pas à 100% pour les joueurs mobile à cause des limites sur mobile)
                 Cheat Indicator is required for all completions via Geode, MegaHack, or iCreate Pro. If you do not have Cheat Indicator on, your record will likely be invalidated (this is not 100% required for mobile as of yet due to mobile limitations).
             </p>
         </div>
